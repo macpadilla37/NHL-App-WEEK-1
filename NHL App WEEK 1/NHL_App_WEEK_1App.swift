ae21dbb12d3751cb4403b93446742241e0ac7fe8
macpadilla37 <89860555+macpadilla37@users.noreply.github.com>
2023-10-25 22:44:28 -0600

    Initial Commit

---
+++ NHL App WEEK 1/NHL_App_WEEK_1App.swift
@@ -0,0 +1,17 @@
+//
+//  NHL_App_WEEK_1App.swift
+//  NHL App WEEK 1
+//
+//  Created by Mac Padilla on 10/25/23.
+//
+
+import SwiftUI
+
+@main
+struct NHL_App_WEEK_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
